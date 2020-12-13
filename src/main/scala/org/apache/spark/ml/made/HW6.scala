e812d48ef7cb4821688e1728d55c1e1705f9d0d7
Oleg Pozovnoy <pozovnoy.oleg@gmail.com>
2020-12-13 22:55:09 +0300

    initial commit

---
+++ src/main/scala/org/apache/spark/ml/made/HW6.scala
@@ -0,0 +1,51 @@
+package org.apache.spark.ml.made
+
+import breeze.linalg.normalize
+import org.apache.spark.ml.linalg.{BLAS, Vector, Vectors}
+
+import scala.util.Random
+
+object HW6 extends App {
+  override def main(args: Array[String]): Unit = {
+
+    val rand = new Random(0)
+    /////////////////////////////////////////////////////////////////////////////////////////
+    var elems1: Vector = Vectors.dense(1, 2, 3)
+    var elems2: Vector = Vectors.dense(0, 1, 0)
+
+    val randUnitVectors: Array[Vector] = {
+      Array.fill(10) {
+        val randArray = Array.fill(3)(rand.nextGaussian())
+        Vectors.fromBreeze(normalize(breeze.linalg.Vector(randArray)))
+      }
+    }
+    println("Randunitvecc")
+    println(randUnitVectors.deep.mkString("\n"))
+
+    var hashValues1 = randUnitVectors.map(
+      randUnitVector => Math.signum(BLAS.dot(elems1, randUnitVector)/ Vectors.norm(elems1,2) )
+    )
+
+    var hashValues2 = randUnitVectors.map(
+      randUnitVector =>Math.signum(BLAS.dot(elems2, randUnitVector)/ Vectors.norm(elems2,2) )
+    )
+
+
+    var x1: Seq[Vector] = (hashValues1.map(Vectors.dense(_)))
+    var y1: Seq[Vector] = (hashValues2.map(Vectors.dense(_)))
+
+    println("vectors")
+    println(x1)
+    println(y1)
+
+    val distance  =  x1.zip(y1).map(vectorPair =>
+      vectorPair._1.toArray.zip(vectorPair._2.toArray).count(pair => pair._1 != pair._2))
+    println("distance")
+    println(distance)
+
+    val result  = x1.zip(y1).map(vectorPair => Vectors.sqdist(vectorPair._1, vectorPair._2))
+
+    println(result.sum.toDouble/result.length)
+
+  }
+}
